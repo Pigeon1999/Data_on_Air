1350ddf991c640d6dcaa7d64045ac704497245dc
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-07-20 17:16:57 +0900

    Update Crawling_data.py

--- Crawling_data.py
+++ Crawling_data.py
@@ -1,5 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
+import pandas as pd 
 
 # 인증키 
 api_key = '75546c586b70617338367a5858524c'
@@ -7,9 +9,15 @@ url = f'http://openapi.seoul.go.kr:8088/{api_key}/xml/citydata/1/1/POI006'
 
 # 결과값 출력 
 res = requests.get(url)
-soup = BeautifulSoup(res.text, 'lxml')
-print(soup.prettify()) # 들여쓰기 출력 
+data = res.content
+root = ET.fromstring(data)
 
-# xml형식 필요한 columns만 가져오고 DataFrame으로 만들기 
+data_list = []
 
-# DataFrame을 sql로 저장
+for item in root.findall('CITYDATA'):  # 'item' 태그에 데이터가 있는 경우
+    AREA_NM = item.find('AREA_NM').text
+    AREA_CD = item.find('AREA_CD').text
+    data_list.append({'AREA_NM': AREA_NM, 'AREA_CD' : AREA_CD})   
+    
+df = pd.DataFrame(data_list)
+print(df)
