eafe1316a8f9fabdaf802b2c20f6f61db8619526
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-07-20 11:16:04 +0900

    Update Crawling_data.py

--- Crawling_data.py
+++ Crawling_data.py
@@ -2,9 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 # 인증키 
-apikey = '75546c586b70617338367a5858524c'
-url = f'http://openapi.seoul.go.kr:8088/{apikey}/xml/citydata/1/1/POI006'
+api_key = '75546c586b70617338367a5858524c'
+url = f'http://openapi.seoul.go.kr:8088/{api_key}/xml/citydata/1/1/POI006'
 
+# 결과값 출력 
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'lxml')
-print(soup.prettify())
+print(soup.prettify()) # 들여쓰기 출력 
+
+# xml형식 필요한 columns만 가져오고 DataFrame으로 만들기 
+
+# DataFrame을 sql로 저장
