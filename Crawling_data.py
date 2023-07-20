2dfbb89273bf5b356a6a36fc5eab5a234e8464b5
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-07-20 11:06:53 +0900

    Update Crawling_data.py

--- Crawling_data.py
+++ Crawling_data.py
@@ -1,10 +1,11 @@
 import requests
-import json
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 # 인증키 
 apikey = '75546c586b70617338367a5858524c'
-url = f'http://openapi.seoul.go.kr:8088/{apikey}/json/citydata_ppltn/1/10/강남 MICE 관광특구'
+url = f'http://openapi.seoul.go.kr:8088/{apikey}/xml/citydata/1/1/POI006'
 
-# request 결과 json으로 출력
-response = requests.get(url).json()
-print(json.dumps(response, indent = 2, ensure_ascii = False))
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'lxml')
+print(soup.prettify())
