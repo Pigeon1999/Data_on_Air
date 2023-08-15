189376c8afb40f23f44c4cd6dae0654669de7418
Pigeon <pass599963@gmail.com>
2023-08-15 23:30:52 +0900

    Update keyword_data.py

--- keyword_data.py
+++ keyword_data.py
@@ -32,17 +32,22 @@ from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 nltk.download('punkt')
 
 # 초기 설정
-class pre_process:
-    def __init__(self, df):
+class pre_process: 
+
+    x_train = []
+    x_test = []
+    y_train = []
+    y_test = []
+    vocab_size = 0
+    embedding_dim = 0
+    
+    def __init__(self, csv):
         #os.chdir('py-hanspell')
         self.okt = Okt()
-        self.df = df
+        self.df = csv
 
 # 1. 주제 없애고 label까지 처리        
 class label(pre_process):
-    def __init__(self, df):
-        super().__init__(df)
-    
     def label_processing(self):
         self.df.dropna(axis=0, inplace=True) # 결측치 제거
         idx = self.df.loc[self.df['label']=='판단 유보'].index
@@ -60,9 +65,6 @@ class label(pre_process):
     
 # 2. '내용, 상세내용'의 특수문자 제거, 불용어 제거, 맞춤법 조정       
 class text(pre_process):
-    def __init__(self, df):
-        super().__init__(df)
-        
     # 2-1 특수문자 제거
     def remove_special_characters(self, text):
         # 한글, 알파벳, 숫자, 공백을 제외한 문자 제거
@@ -155,10 +157,7 @@ class text(pre_process):
             
         return self.df 
     
-class token(pre_process):   
-    def __init__(self, df):
-        super().__init__(df)
-        
+class token(pre_process):  
     # '상세내용' 토큰화
     def tokenizer(self, text):
         morph = self.okt.pos(text)
@@ -197,26 +196,147 @@ class token(pre_process):
 
         return self.df
 
-def main(df):
-    Label = label(df)
-    Text = text(df)
-    Token = token(df)
+# 4. 워드 임베딩 
+class Word_Embedding(pre_process):      
+    def word_embedding(self):
+        # Word2Vec 모델로 학습된 임베딩 벡터 가져오기
+        model = Word2Vec(sentences = self.df['상세내용'], vector_size = 100, window = 5, min_count = 3, workers = 4, sg = 0)
+        embedding_matrix = model.wv.vectors
+        pre_process.vocab_size, pre_process.embedding_dim = embedding_matrix.shape
 
-    df = Label.label_processing()
-    df = Text.text_processing()
-    df = Token.token_processing()
+        x = self.df['상세내용']
+        y = self.df['label']
+
+        pre_process.x_train, pre_process.x_test, pre_process.y_train, pre_process.y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+        # 정수 인코딩
+        tokenizer = Tokenizer(pre_process.vocab_size, oov_token = 'OOV')
+        tokenizer.fit_on_texts(pre_process.x_train)
+        pre_process.x_train = tokenizer.texts_to_sequences(pre_process.x_train)
+        pre_process.x_test = tokenizer.texts_to_sequences(pre_process.x_test)
+        
+        # 패딩 : 샘플들의 길이를 동일하게 맞춰줌
+        print('리뷰의 최대 길이 :',max(len(review) for review in pre_process.x_train))
+        print('리뷰의 평균 길이 :',sum(map(len, pre_process.x_train))/len(pre_process.x_train))
+        
+        max_len = max(len(review) for review in pre_process.x_train)
+        while True:
+            count = 0
+            for sentence in pre_process.x_train:
+                if(len(sentence) <= (max_len // 10) * 10):
+                    count = count + 1
+                
+            rate = (count / len(pre_process.x_train)) * 100
+            if rate >= 99:
+                print('전체 샘플 중 길이가 %s 이하인 샘플의 비율: %s'%(max_len, (count / len(pre_process.x_train))*100))
+                print('모든 샘플의 길이 %s로 패딩'%(max_len))
+                pre_process.x_train = pad_sequences(pre_process.x_train, maxlen=max_len)
+                pre_process.x_test = pad_sequences(pre_process.x_test, maxlen=max_len)
+
+                # label 타입 변경
+                pre_process.y_train = np.array(pre_process.y_train).astype(np.float32)
+                pre_process.y_test = np.array(pre_process.y_test).astype(np.float32)
+                break
+            else:
+                max_len = max_len + 1
+   
+# 5. BiLSTM 모델 학습 
+class BiLSTM(pre_process):
+    def make_BiLSTM(self):
+        # BiLSTM 모델 구성
+        hidden_units = 128  # LSTM 은닉 상태 크기
+        output_classes = 2  # 분류할 클래스 수
+        max_sequence_length = 3200
+
+        model = Sequential()
+        model.add(Embedding(input_dim = pre_process.vocab_size, output_dim = pre_process.embedding_dim, input_length = None))  # 임베딩 레이어
+        model.add(Bidirectional(LSTM(hidden_units, return_sequences = True)))  # 양방향 LSTM 레이어
+        model.add(Bidirectional(LSTM(hidden_units)))  # 양방향 LSTM 레이어
+        model.add(Dense(output_classes, activation = 'softmax'))  # 출력 레이어
+
+        es = EarlyStopping(monitor = 'val_loss', mode = 'min', verbose = 1, patience = 4)
+        mc = ModelCheckpoint('best_model.h5', monitor = 'val_acc', mode = 'max', verbose = 1, save_best_only = True)
+
+        model.compile(loss = 'sparse_categorical_crossentropy', optimizer = 'adam', metrics = ['accuracy'])
+
+        # 모델 학습
+        trained_model = model.fit(pre_process.x_train, pre_process.y_train, epochs = 15, callbacks=[es, mc], batch_size = 256, validation_split = 0.2)
+        model.save('trained_BiLSTM_model') 
+        loaded_model = load_model('trained_BiLSTM_model')   
+        print("테스트 정확도: %.4f" % (loaded_model.evaluate(pre_process.x_test, pre_process.y_test)[1]))
     
-    return df
+def make_model():
+    csv = pd.read_csv('/content/SNU_factcheck_sample.csv', encoding = 'cp949')
+    Pre_process = pre_process(csv)
+    df = Pre_process.df
+
+    Label = label(df)
+    df = Label.label_processing()
+
+    Text = text(df)
+    df = Text.text_processing()
+
+    Token = token(df)
+    df = Token.token_processing()
+
+    word_embedding = Word_Embedding(df)
+    word_embedding.word_embedding()
+
+    bilstm = BiLSTM(df)
+    bilstm.make_BiLSTM()
+
+def predict_model(x_test, y_test):
+    loaded_model = load_model('trained_BiLSTM_model')  
+    correct_data = 0
+    score = loaded_model.predict(x_test) # 예측
+    for i in range(0, len(x_test)):
+      if(score[i][1] > 0.5):
+        print(f"{score[i][1] * 100:.2f}% 확률로 참입니다. <실제 판단 여부 : {y_test[i]}>")
+        if y_test[i] == 1:
+          correct_data = correct_data + 1
+      else:
+        print(f"{(1 - score[i][1]) * 100:.2f}% 확률로 거짓입니다. <실제 판단 여부 : {y_test[i]}>")
+        if y_test[i] == 0:
+          correct_data = correct_data + 1
+    print(f'정답률 {len(y_test)}개중 {correct_data}개 정답.')
+    print(f'{correct_data/len(y_test) * 100:.2f}%')
 
 ''' 
+1. make_model 함수
 [실행코드]
-df = pd.read_csv('D:\DownLoad\SNU_factcheck_sample.csv', encoding = 'cp949')
-df = main(df)
-print(df)
+main()
 
 [결과]
    row_id	주제	내용	                                                        상세내용	                                                               주장/검증 매체	          label
 0	0.0	    경제	직원 16만 명인데 임원 14만 명 새마을금고 조직구조 희한하다	        [한국, 신문, 새마을금고, 부실, 사태, 관련, 방만, 경영, 관리, 부실, 사태...	   언론보도	                 전혀 사실 아님
 1	1.0	    경제	탄소 포집 기술은 재생에너지보다 경제성이 뛰어난 탄소중립 대안이다	 [탄소, 중립, 녹색, 성장, 위원회, 지난, 탄소, 중립, 녹색, 성장, 계획, ...	    조원동	                  전혀 사실 아님
 2	2.0	    경제	탄소 포집 기술은 2030년 이전에 상용화돼 탄소중립에 기여할 수 있다	 [확정, 정부, 탄소, 중립, 녹색, 성장, 계획, 활용, 연간, 온실가스, 감축,...	    언론사 자체 문제제기	   대체로 사실 아님
+
+리뷰의 최대 길이 : 372
+리뷰의 평균 길이 : 351.5
+전체 샘플 중 길이가 380 이하인 샘플의 비율: 100.0
+모든 샘플의 길이 380로 패딩
+Epoch 1/15
+1/1 [==============================] - ETA: 0s - loss: 0.7009 - accuracy: 0.0000e+00WARNING:tensorflow:Can save best model only with val_acc available, skipping.
+1/1 [==============================] - 12s 12s/step - loss: 0.7009 - accuracy: 0.0000e+00 - val_loss: 0.6153 - val_accuracy: 1.0000
+
+...
+
+Epoch 15/15
+1/1 [==============================] - ETA: 0s - loss: 0.0000e+00 - accuracy: 1.0000WARNING:tensorflow:Can save best model only with val_acc available, skipping.
+1/1 [==============================] - 0s 90ms/step - loss: 0.0000e+00 - accuracy: 1.0000 - val_loss: 0.0000e+00 - val_accuracy: 1.0000
+WARNING:absl:Found untraced functions such as lstm_cell_31_layer_call_fn, lstm_cell_31_layer_call_and_return_conditional_losses, lstm_cell_32_layer_call_fn, lstm_cell_32_layer_call_and_return_conditional_losses, lstm_cell_34_layer_call_fn while saving (showing 5 of 8). These functions will not be directly callable after loading.
+1/1 [==============================] - 2s 2s/step - loss: 0.0000e+00 - accuracy: 1.0000
+테스트 정확도: 1.0000
+
+2. predict_model 함수
+[실행코드]
+predict_model(pre_process.x_test, pre_process.y_test) --> 테스트 데이터 x, y
+
+[결과]
+1/1 [==============================] - 2s 2s/step
+100.00% 확률로 거짓입니다. <실제 판단 여부 : 0.0>
+정답률 1개중 1개 정답.
+100.00%
+
 '''
