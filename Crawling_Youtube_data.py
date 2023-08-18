7b29b5ff01f72094c86e8b5f6febb4e8f8ed6736
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-08-18 18:55:14 +0900

    Update Crawling_Youtube_data.py

--- Crawling_Youtube_data.py
+++ Crawling_Youtube_data.py
@@ -1,6 +1,3 @@
-#!pip install youtube_transcript_api
-#!pip install selenium
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
