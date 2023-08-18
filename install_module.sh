62f809e7b35b49f5dfa2582721c254ca95b6098e
Pigeon1999 <98893114+Pigeon1999@users.noreply.github.com>
2023-08-18 17:53:48 +0900

    Update install_module.sh

--- install_module.sh
+++ install_module.sh
@@ -5,7 +5,7 @@ pip install tensorflow==2.11
 pip install keras==2.11.0
 pip install konlpy==0.6.0
 pip install gensim==4.3.1
-pip install scikit-learn==1.3.0
+pip install --only-binary :all: scikit-learn==1.3.0
 pip install selenium==4.10.0
 pip install beautifulsoup4==4.12.2
 pip install youtube-transcript-api==0.6.1
