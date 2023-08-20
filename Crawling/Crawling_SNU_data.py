c7141287bf2e33c239f6e0bb8ca36f8bd2f43226
Pigeon <pass599963@gmail.com>
2023-08-21 01:05:19 +0900

    Update Crawling_SNU_data.py

--- Crawling/Crawling_SNU_data.py
+++ Crawling/Crawling_SNU_data.py
@@ -16,7 +16,10 @@ class Dynamic_Page:
         self.chrome_options.add_argument('--headless')
         self.chrome_options.add_argument('--no-sandbox')
         self.chrome_options.add_argument('--disable-dev-shm-usage')
-        self.dr = webdriver.Chrome(options = self.chrome_options)
+        try:
+            self.dr = webdriver.Chrome(options = self.chrome_options)
+        except:
+            self.dr = webdriver.Chrome('chromedriver', options = self.chrome_options)
         self.dr.get(self.url)  # 드라이버를 통해 url의 웹 페이지를 오픈
         time.sleep(4)
 
